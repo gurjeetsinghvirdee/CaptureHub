364903cf59ebb20742f7fcecd208dfe8bcb5a0f6
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-11-08 20:26:33 +0530

    shadcn ui installed

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Button } from "src/components/ui/button"
 
 const SignupForm = () => {
   return (
     <div>
-      
+      <Button>Hit Me</Button>
     </div>
   )
 }
